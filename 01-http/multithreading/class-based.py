0d0e00618ccc3d5c2f6effcd80a3a54c159e07c3
Ivo Donchev <i.donchev@hacksoft.io>
2021-05-08 11:43:43 +0300

    Add class based example for multithreading

---
+++ 01-http/multithreading/class-based.py
@@ -0,0 +1,49 @@
+import time
+import requests
+import threading
+
+
+def http_get(url):
+    response = requests.get(url)
+    print(f'HTTP GET {url} - response {response.status_code}')
+    return response
+
+
+class AsyncHttpGet(threading.Thread):
+    def run(self):
+        http_get('https://abv.bg/')
+
+
+def singlethreaded():
+    for i in range(20):
+        http_get('https://abv.bg/')
+
+
+def multithreaded():
+    threads = []
+
+    # Start 10 separate threads
+    for i in range(20):
+        thread = AsyncHttpGet()
+        thread.start()
+        threads.append(thread)
+
+    # Wait for all the threads to finish
+    for thread in threads:
+        thread.join()
+
+
+def main():
+    print('Running....')
+
+    start = time.time()
+    singlethreaded()
+    print('Without threads: ', time.time() - start)
+
+    start = time.time()
+    multithreaded()
+    print('With threads: ', time.time() - start)
+
+
+if __name__ == '__main__':
+    main()
